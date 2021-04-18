fc4635a4950b7efe931fb88a4f88ce82859c142f
addy michael <addymichael18@outlook.com>
2021-04-18 04:03:37 +0100

    alert and search bar added

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,39 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import UserItem from "./UserItem";
+import Spinner from "../layouts/Spinner";
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "mojombo",
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: "2",
-        login: "defunkt",
-        avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt",
-      },
-      {
-        id: "3",
-        login: "wycats",
-        avatar_url: "https://avatars.githubusercontent.com/u/4?v=4",
-        html_url: "https://github.com/wycats",
-      },
-    ],
-  };
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 const userStyle = {
   display: "grid",
