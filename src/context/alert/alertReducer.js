53dc7baff79cf6bd64f5bb303150361c71b747ac
addy michael <addymichael18@outlook.com>
2021-04-22 02:57:15 +0100

    preparation completed

--- src/context/alert/alertReducer.js
+++ src/context/alert/alertReducer.js
@@ -3,7 +3,6 @@ import { SET_ALERT, REMOVE_ALERT } from "../types";
 export default (state, action) => {
   switch (action.type) {
     case SET_ALERT:
-      console.log(action.payload);
       return action.payload;
 
     case REMOVE_ALERT:
